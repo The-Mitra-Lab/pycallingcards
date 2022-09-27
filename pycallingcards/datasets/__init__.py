6bcf396b7134560ebfae19052caf9e5f86ccf454
Juanru Guo <64772552+JuanruMaryGuo@users.noreply.github.com>
2022-09-26 21:06:23 -0500

    update

--- pycallingcards/datasets/__init__.py
+++ pycallingcards/datasets/__init__.py
@@ -1,3 +1,3 @@
 """Single-cell calling cards Python"""
 
-from pycallingcards.datasets._datasets import mousecortex_data,SP1_K562HCT116_data,mouse_brd4_data
+from pycallingcards.datasets._datasets import mousecortex_data,SP1_K562HCT116_data,mouse_brd4_data,SP1_Cre_data
